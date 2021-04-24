4624cf76384902ada5483b1bdb4d702e6a4110e9
Don2Quixote <idonquixote@yandex.ru>
2021-04-24 16:50:18 +0300

    100% code coverage

--- ninja_test.go
+++ ninja_test.go
@@ -11,6 +11,47 @@ import (
 	. "github.com/don2quixote/ninjago"
 )
 
+func TestCreateRouterWithRoutesOverflow1(t *testing.T) {
+	defer func() {
+		if p := recover(); p == nil {
+			t.Fatal("Expected to panic due to routes overflow")
+		}
+	}()
+	router := CreateRouter(0, 0)
+	router.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {})
+}
+
+func TestCreateRouterWithRoutesOverflow2(t *testing.T) {
+	defer func() {
+		if p := recover(); p == nil {
+			t.Fatal("Expected to panic due to routes overflow")
+		}
+	}()
+	router := CreateRouter(0, 0)
+	router.Handle("", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+}
+
+func TestCreateRouterWithMiddlewaresOverflow(t *testing.T) {
+	defer func() {
+		if p := recover(); p == nil {
+			t.Fatal("Expected to panic due to middlewares overflow")
+		}
+	}()
+	router := CreateRouter(0, 0)
+	router.SetMiddleware("", ThroughMiddleware(func(w http.ResponseWriter, r *http.Request) {}))
+}
+
+func TestCreateRouterWithNoOverflows(t *testing.T) {
+	defer func() {
+		if p := recover(); p != nil {
+			t.Fatal("Not expected to panic - no overflows")
+		}
+	}()
+	router := CreateRouter(1, 1)
+	router.SetMiddleware("", ThroughMiddleware(func(w http.ResponseWriter, r *http.Request) {}))
+	router.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {})
+}
+
 func TestServeHTTP(t *testing.T) {
 	router1 := CreateRouter(1, 0)
 	router1.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -36,7 +77,7 @@ func TestServeHTTP(t *testing.T) {
 			return false
 		}
 		return true
-	})
+	}).Methods("GET")
 	router3.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
@@ -65,6 +106,38 @@ func TestServeHTTP(t *testing.T) {
 		w.WriteHeader(http.StatusNotFound)
 	})
 
+	router6 := CreateRouter(2, 1)
+	router6.SetMiddleware("//", ThroughMiddleware(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			// Could do some smart sruff here
+		}
+	})).Async()
+	router6.Handle("//", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("secret place"))
+	}))
+	router6.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("not secret"))
+	})
+
+	router7 := CreateRouter(3, 0)
+	router7.HandleFunc("/working", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	router7.HandleFunc("working/no1stslash", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	router7.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	})
+
+	router8 := CreateRouter(3, 0)
+	router8.HandleFunc("/test/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	router8.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	})
+
 	type testingReq struct {
 		path         string
 		method       string
@@ -90,6 +163,7 @@ func TestServeHTTP(t *testing.T) {
 		{router3, []testingReq{
 			{"/test", "GET", http.StatusForbidden, false, ""},
 			{"/test?value=test", "GET", http.StatusOK, false, ""},
+			{"/test", "POST", http.StatusOK, false, ""},
 		}},
 		{router4, []testingReq{
 			{"/", "GET", http.StatusOK, true, "GET"},
@@ -103,12 +177,25 @@ func TestServeHTTP(t *testing.T) {
 			{"/test/hello", "GET", http.StatusOK, true, "hello"},
 			{"/test/kinkong/godzilla", "GET", http.StatusOK, true, "godzilla"},
 		}},
+		{router6, []testingReq{
+			{"/", "GET", http.StatusOK, true, "not secret"},
+			{"//", "POST", http.StatusOK, true, "secret place"},
+		}},
+		{router7, []testingReq{
+			{"/working", "GET", http.StatusOK, false, ""},
+			{"/working/no1stslash", "GET", http.StatusOK, false, ""},
+			{"/", "GET", http.StatusNotFound, false, ""},
+		}},
+		{router8, []testingReq{
+			{"/test/", "GET", http.StatusOK, false, ""},
+			{"/test/mustwork", "GET", http.StatusOK, false, ""},
+		}},
 	}
 
 	for _, test := range testCases {
 		for _, req := range test.reqs {
 			w := httptest.NewRecorder()
-			r, err := http.NewRequest(req.method, fmt.Sprintf("http://localhost%s", req.path), nil)
+			r, err := http.NewRequest(req.method, fmt.Sprintf("http://127.0.0.1%s", req.path), nil)
 			if err != nil {
 				t.Fatalf("Unexpected error: %s", err.Error())
 			}
